a4fe95a291d577c8671c55311e06fcc5911ea9de
hiroaki <original.taste.co@gmail.com>
2022-11-04 19:36:34 +0900

    ログイン時のフラッシュメッセージを追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,8 +4,10 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      flash[:success] = 'Login successful.'
       redirect_back_or_to root_path
     else
+      flash[:danger] = 'Login failed.'
       render :new
     end
   end
